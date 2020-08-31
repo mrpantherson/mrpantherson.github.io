c87a466ef92c519f4e85db76dc65bdcee06d49cf
brandon <oldmanpatterson@gmail.com26782348+mrpantherson@users.noreply.github.com>
2020-08-31 14:29:36 -0400

    Added another Altair graph

--- src/samples.py
+++ src/samples.py
@@ -2,9 +2,8 @@ import altair as alt
 import pandas as pd
 
 
-df = pd.read_csv('./data/dnd_monsters.csv')
-
 # dnd scatter
+df = pd.read_csv('./data/dnd_monsters.csv')
 
 chart = alt.Chart(df).mark_point().encode(
     alt.X('hp', scale=alt.Scale(zero=False)),
@@ -18,3 +17,19 @@ chart = chart.properties(title='DnD Monsters 5e',
     height=600, )
 
 chart.save('./graphs/graph1.html')
+
+# dnd align heatmap
+df = pd.read_csv('./data/dnd_monsters.csv')
+df = df.loc[:, ['align', 'cr']]
+
+chart = alt.Chart(df).mark_rect().encode(
+    x=alt.X('cr:O', title="challenge rating"),
+    y=alt.Y('align:O', title=''),
+    color=alt.Color('count():Q')
+    )
+
+chart = chart.properties(title='DnD Monsters 5e - Count of Alignment Per Challenge Rating',
+    width=1000,
+    height=600, )
+
+chart.save('./graphs/graph2.html')
